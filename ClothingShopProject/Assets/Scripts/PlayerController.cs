ef4b20f67d690341799d8ab2079785e0ab630d47
Rodrigo Barbosa Caitano da Silva <68601982+Rodwigo@users.noreply.github.com>
2021-10-04 12:54:23 -0300

    add player controller and cinemachine

---
+++ ClothingShopProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer sprite;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.x != 0 || movement.y != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else { anim.SetBool("isWalking", false); }
+
+        if(movement.x < 0 || movement.y < 0)
+        {
+            sprite.flipX = true;
+        }
+        if (movement.x > 0 || movement.y > 0)
+        {
+            sprite.flipX = false;
+        }
+    }
+ 
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
